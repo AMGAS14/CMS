1213ecb7ae4990285522488bdb9bc2e1cf87c9d8
Antonio Antunes <amgas14@gmail.com>
2021-04-08 15:38:31 +0100

    Student Class

---
+++ ServerApp/Models/Student.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ServerApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public date birthday { get; set; }  
+        public List<Grades> getGrades { get; set; }
+    }
+}
